a98fdae5769c2c9e87007da957b9c35b5eb0e315
Saurabh Sonar <saurabh.sonar@exprealty.net>
2024-08-30 14:26:22 +0530

    completed drag drop functionality

---
+++ lib/widget/one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class one extends StatelessWidget {
+  const one({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.brown,
+      height: 200,
+      width: double.infinity,
+      child: Text("one"),
+    );
+  }
+}
